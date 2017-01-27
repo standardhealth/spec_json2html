b3b17f7813d01e8f027b0ec42b03377119f0e97b
Dylan Phelan <dtphelan1@gmail.com>
2017-01-27 09:18:53 -0500

    successful rendering of a template (only possible by removing helper usage; TODO: reincorpoarte getnamespacefilename)

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,13 +15,7 @@ String.prototype.capitalize = function() {
 module.exports = function(grunt) {
 'use strict';
 
-  // Delete after first run
-  if(!grunt.file.exists('vendor')) {
-    grunt.fail.fatal('>> Please run "bower install" before continuing.');
-  }
   var addHierarchyIndex = function(children,hierarchy) {
-  //  console.log(children);
-  var hierarchy = hierarchy;
     _.each(children,function(item) {
       if (item && _.isObject(item)) {
         if (item.type == "Identifier") {
@@ -46,6 +40,7 @@ module.exports = function(grunt) {
   // Set up the top-level pages for each namespace
   var spec_template = grunt.file.read('./templates/pages/namespace.hbs');
   console.log("spec_template = " + JSON.stringify(spec_template));
+
   var data = grunt.file.readJSON('./data/hierarchy.json');
   var namespaces = _.indexBy(data.children,"label");
   _.map(data.children,function(namespace) {
@@ -53,7 +48,6 @@ module.exports = function(grunt) {
   });
   data.index = namespaces;
   addHierarchyIndex(data.children,data);
- // console.log(JSON.stringify(data.index))
 
   var namespace_pages = _.map(data.children,function(item) {
     item.follow=true;
@@ -71,7 +65,6 @@ module.exports = function(grunt) {
     // Project metadata
     pkg:    grunt.file.readJSON('package.json'),
     site:   grunt.file.readYAML('_config.yml'),
-    vendor: grunt.file.readJSON('.bowerrc').directory,
 
     // Before generation, remove files from previous build
     clean: {
@@ -80,8 +73,8 @@ module.exports = function(grunt) {
     copy: {
       main: {
         files: [
-          {expand:true, flatten: true, src: ['templates/includes/**.hbs'], dest:'<%= site.dest %>/<%= site.pages %>/shr/assets/partials'},
-          {expand:true, flatten: true, src: ['data/hierarchy.json'], dest:'<%= site.dest %>/<%= site.pages %>/shr/assets/data'}
+          {expand:true, flatten: true, src: ['templates/includes/**.hbs'], dest:'<%= site.dest %>/<%= site.assets %>/partials'},
+          {expand:true, flatten: true, src: ['data/hierarchy.json'], dest:'<%= site.dest %>/<%= site.assets %>/data'}
         ]
       }
     },
@@ -89,7 +82,7 @@ module.exports = function(grunt) {
     browserify: {
       vendor: {
         src: ['templates/includes/hb_shr.js'],
-        dest: '<%= site.dest %>/<%= site.pages %>/shr/assets/app.js',
+        dest: '<%= site.dest %>/<%= site.assets %>/app.js',
         options: {
           require: ['handlebars']
         }
@@ -114,19 +107,36 @@ module.exports = function(grunt) {
         options : {
           pages:namespace_pages
         },
-        files :[
-          {dest: '<%= site.dest %>/<%= site.pages %>/shr/',src:'!*'}
-        ]
+        files :[{
+          dest: '<%= site.dest %>',
+          src:'!*'
+        }]
       },
       example: {
-        files: {'<%= site.dest %>/': ['<%= site.pages %>/shr/*.hbs']}
+        flatten: true,
+        expand: true,
+        cwd: '<%= site.pages %>/shr',
+        src: '*.hbs',
+        dest: '<%= site.dest %>'
       }
-    }
+    },
+    watch: {
+      scripts: {
+        files: ['**/*.js'],
+        tasks: ['assemble'],
+        options: {
+          spawn: false,
+        },
+      },
+    },
   });
 
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-assemble');
   grunt.registerTask('default',['clean', 'browserify', 'copy', 'assemble']);
+  grunt.registerTask('js', ['browserify'])
+
 }
--- templates/includes/hb_shr.js
+++ templates/includes/hb_shr.js
@@ -1,49 +1,60 @@
 (function() {
   var Handlebars = require('Handlebars');
-  var template = Handlebars.template, 
-      templates = Handlebars.templates = Handlebars.templates || {};
-      templates['schema_shr.hbs'] = template({
-        "1":function(container,depth0,helpers,partials,data,blockParams) {
-          var stack1;
-          // console.log("1: " + JSON.stringify(partials))
-          // console.log(JSON.stringify(container))
-          // // console.log(JSON.stringify(depth0))
-          // console.log(JSON.stringify(helpers))
-          // console.log(JSON.stringify(partials))
-          // // console.log(JSON.stringify(data))
-          // // console.log(JSON.stringify(blockParams))
-          var ret = ((stack1 = helpers["if"].call(depth0 != null ? depth0 : {},blockParams[0][0],{"name":"if","hash":{},"fn":container.program(2, data, 0, blockParams),"inverse":container.noop,"data":data,"blockParams":blockParams})) != null ? stack1 : "");
-          console.log(ret)
-          return(ret);
-        },
-        "2":function(container,depth0,helpers,partials,data) {
-          var stack1;
-          console.log("2: " + JSON.stringify(partials))
-          var ret = ((stack1 = container.invokePartial(partials.schema_namespace_list,depth0,{"name":"schema_namespace_list","data":data,"indent":"    ","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "");
-          // console.log(ret)
-          // console.log('here')
-          return(ret);
-        },
-        "compiler":[7,">= 4.0.0"],
-        "main":function(container,depth0,helpers,partials,data,blockParams) {
-          var stack1;
-          console.log("main: " + JSON.stringify(partials))
-          var ret =  "<h2>Parsing Specification Hierarchy for: "
-                 + container.escapeExpression(container.lambda(((stack1 = (depth0 != null ? depth0.hierarchy : depth0)) != null ? stack1.label : stack1), depth0))
-                 + "</h2>\n\n"
-                 + ((stack1 = helpers.each.call(depth0 != null ? depth0 : {},((stack1 = (depth0 != null ? depth0.hierarchy : depth0)) != null ? stack1.children : stack1),{"name":"each","hash":{},"fn":container.program(1, data, 1, blockParams),"inverse":container.noop,"data":data,"blockParams":blockParams})) != null ? stack1 : "");
-          console.log(ret)
-          return(ret);
-        },
-        "usePartial":true,
-        "useData":true,
-        "useBlockParams":true
-       });
+  // var template = Handlebars.template, 
+  //     templates = Handlebars.templates = Handlebars.templates || {};
+  //     templates['schema_shr.hbs'] = template({
+  //       "1":function(container,depth0,helpers,partials,data,blockParams) {
+  //         var stack1;
+  //         var ret = ((stack1 = helpers["if"].call(depth0 != null ? depth0 : {},
+  //                                                 blockParams[0][0],
+  //                                                 {
+  //                                                   "name":"if",
+  //                                                   "hash":{},
+  //                                                   "fn":container.program(2, data, 0, blockParams),
+  //                                                   "inverse":container.noop,
+  //                                                   "data":data,
+  //                                                   "blockParams":blockParams})) 
+  //                               != null ? stack1 : "");
+  //         return(ret);
+  //       },
+  //       "2":function(container,depth0,helpers,partials,data) {
+  //         var stack1;
+  //         console.log(depth0)  ;
+  //         var ret = ((stack1 = container.invokePartial(partials.schema_namespace_list,
+  //                                                      depth0,
+  //                                                      {"name":"schema_namespace_list",
+  //                                                      "data":data,
+  //                                                      "indent":"    ",
+  //                                                      "helpers":helpers,
+  //                                                      "partials":partials,
+  //                                                      "decorators":container.decorators})) 
+  //                     != null ? stack1 : "");
+  //         return(ret);
+  //       },
+  //       "compiler":[7,">= 4.0.0"],
+  //       "main":function(container,depth0,helpers,partials,data,blockParams) {
+  //         var stack1;
+  //         var ret =  "<h2>Parsing Specification Hierarchy for: "
+  //                + container.escapeExpression(container.lambda(((stack1 = (depth0 != null ? depth0.hierarchy : depth0)) != null ? stack1.label : stack1), depth0))
+  //                + "</h2>\n\n"
+  //                + ((stack1 = helpers.each.call(depth0 != null ? depth0 : {},((stack1 = (depth0 != null ? depth0.hierarchy : depth0)) != null ? stack1.children : stack1),{"name":"each","hash":{},"fn":container.program(1, data, 1, blockParams),"inverse":container.noop,"data":data,"blockParams":blockParams})) != null ? stack1 : "");
+  //         return(ret);
+  //       },
+  //       "usePartial":true,
+  //       "useData":true,
+  //       "useBlockParams":true
+  //      });
 
   String.prototype.capitalize = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
   }
   $(document).ready(function() {
+    registerPartials();
+    registerHelpers();
+
+  });
+
+
     $("#load_hierarchy").click(function() {
       $.getJSON("assets/data/hierarchy.json",function(data) {
         window.hierarchy = data;
@@ -53,21 +64,23 @@
         });
         hierarchy.index = namespaces;
         addHierarchyIndex(hierarchy.children);
+    console.log('fin addhier')
 
         /* Call Handlebars.compile on the templates
          and then call the template with hierarchy as the context */
-         // console.log(Handlebars);
-         // console.log(Handlebars.helpers);
-         // console.log(Handlebars.partials);
-         // console.log(Handlebars.template);
-         // addPartials();
-         console.log(hierarchy);
+        var template = $.ajax({
+            url: $('#schema_shr').attr('src'),
+            type: "GET", 
+            async: false
+          });
+        console.log(template.responseText)
 
-         console.log(templates['schema_shr.hbs']({hierarchy}));
+        var tempScript = Handlebars.compile(template.responseText);
+        console.log(Handlebars.partials);
+        var html = tempScript({hierarchy: hierarchy});
+        console.log(html);
       });
-
     });
-  });
   // function addPartials() {
   //   $.load('/assets/templates', function (data) {
   //     console.log(data)
@@ -97,6 +110,44 @@
         // not an object, not an array
         // console.log(item);
       }
+    });
+  }
+
+  function registerPartials(templates, hierarchy) { 
+    var dfd = $.Deferred(); 
+    
+    dfd.done(function () { 
+      $.each($('#partials script').get(), function(index, value) {
+        var partial = $('#partials script').get(index)
+        if (partial != 'schema_shr') { 
+          var id = $(partial).attr('id')
+          $.get($(partial).attr('src'), function(data, status) {
+            if (status == "success") { 
+              Handlebars.registerPartial(id, data);
+            } else { 
+              console.log('Error loading partial with ID: ' + id + "\nError: " + err);
+            }
+          })
+        }
+      });
+    }).done(function () {
+      console.log('finished registering partials')
+    }) 
+    dfd.resolve();
+  }
+
+  function registerHelpers () { 
+    
+    Handlebars.registerHelper('getNamespaceFilename', function(name,context) {
+      return new Handlebars.SafeString(name.split('.')[1]+'.html');
+    });
+
+    Handlebars.registerHelper('eq', function(a, b, opts) {
+        if (a == b) {
+            return opts.fn(this);
+        } else {
+            return opts.inverse(this);
+        }
     });
   }
 })();
