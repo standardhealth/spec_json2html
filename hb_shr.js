469dea519294f3e7f850d064bc9a4dce7b2a7582
Nicole ng <nng@mitre.org>
2017-04-07 16:01:12 -0400

    Added functionality to redirect user to data element page when selecting the element in the search bar

--- hb_shr.js
+++ hb_shr.js
@@ -1,72 +1,83 @@
-(function () { 
+// Keep a model of the important data alive in the app (Dylan)
+var App = window.App = {
+    // namespaces: [],
+    // dataelements: [],
+    // valuesets: [],
+    // codesystems: [],
+    options: [],
+    selectedElem: {}
+};
+
+(function () {
     const Fuse = require('fuse.js');
     const _ = require('lodash');
     // Define capitalize incase it's not already
-    String.prototype.capitalize = function() {
-      return this.charAt(0).toUpperCase() + this.slice(1);
+    String.prototype.capitalize = function () {
+        return this.charAt(0).toUpperCase() + this.slice(1);
     }
     //
     //
     // Format a list to be searched by Fuse.js
     //
-    function formatForSearch (hier, list) { 
-        _.forEach(hier.children, function (shrArea) { 
+    function formatForSearch(hier, list) {
+        _.forEach(hier.children, function (shrArea) {
             // shrArea can contain namespaces, valusets, or codesystems
             let sectionType = shrArea.type,
-                ns, 
-                link; 
-            _.forEach(shrArea.children, function (subSection) { 
+                ns,
+                link;
+            _.forEach(shrArea.children, function (subSection) {
                 // Subsection can be an individual namespace, valueset, or codesystem
-                switch (sectionType) { 
-                    case "ValueSets": 
+                switch (sectionType) {
+                    case "ValueSets":
                         // We want to search on valueset names, nothing else
                         link = `/shr/${subSection.namespace.split('.')[1].capitalize()}/vs/#${subSection.label}`;
                         addObjToSearch(subSection.label,
-                                       subSection.description, 
-                                       sectionType,
-                                       subSection.namespace.split('.')[1].capitalize(), 
-                                       link,
-                                       list);
+                            subSection.description,
+                            sectionType,
+                            subSection.namespace.split('.')[1].capitalize(),
+                            link,
+                            list);
                         break;
-                    case "CodeSystems": 
+                    case "CodeSystems":
                         // We want to search on individual codes
                         ns = subSection.namespace;
-                        _.forEach(subSection.children, function (curElement) { 
+                        _.forEach(subSection.children, function (curElement) {
                             link = `/shr/${ns.split('.')[1].capitalize()}/cs/#${curElement.code}`;
                             // Current Elements can be an individual codes contained in a codesystem
                             // N.B. the code servs the role of label, and display servces the role of description.
-                            addObjToSearch(curElement.code, 
-                                           curElement.display,
-                                           sectionType, 
-                                           ns.split('.')[1].capitalize(),  
-                                           link,
-                                           list);
-                        }); 
+                            addObjToSearch(curElement.code,
+                                curElement.display,
+                                sectionType,
+                                ns.split('.')[1].capitalize(),
+                                link,
+                                list);
+                        });
                         break;
                     case "Namespaces":
                         // We want to search on namespace names
                         ns = subSection.label;
                         link = `/shr/${ns.split('.')[1].capitalize()}/`;
-                        addObjToSearch(subSection.label, 
-                                       subSection.description, 
-                                       sectionType, 
-                                       ns.split('.')[1].capitalize(), 
-                                       link,
-                                       list);
-                        _.forEach(subSection.children, function (curElement) { 
+                        addObjToSearch(subSection.label,
+                            subSection.description,
+                            sectionType,
+                            ns.split('.')[1].capitalize(),
+                            link,
+                            list);
+                        _.forEach(subSection.children, function (curElement) {
                             link = `/shr/${ns.split('.')[1].capitalize()}/#${curElement.label}`;
                             // We want to be able to search each individaul data element
                             addObjToSearch(curElement.label,
-                                           curElement.description, 
-                                           curElement.type,
-                                           ns.split('.')[1].capitalize(), 
-                                           link,
-                                           list);
-                        }); 
+                                curElement.description,
+                                curElement.type,
+                                ns.split('.')[1].capitalize(),
+                                link,
+                                list);
+                        });
                         break;
-                    default: 
+                    default:
                         console.log('New type: ' + sectionType);
-                };
+                }
+                ;
             });
         });
     };
@@ -74,15 +85,16 @@
     //
     // Short function for pushing a new object onto list of searchable objects for Fuse.
     //
-    function addObjToSearch (label, description, ty, ns, link, list) { 
-        let curObj            = {};
-        curObj['label']       = label;
+    function addObjToSearch(label, description, ty, ns, link, list) {
+        let curObj = {};
+        curObj['label'] = label;
+        curObj['link'] = link;
         curObj['description'] = description;
-        curObj['type']        = ty;
-        curObj['ns']          = ns;
+        curObj['type'] = ty;
+        curObj['ns'] = ns;
         // Different html formats
-        curObj['html']        = `<option href='${link}' value='${label}'>${ty}</option>`;
-        // curObj['html']        = `<li><a href='${link}' value='${ty}'>${label}</a></li>`;
+        // curObj['html'] = `<option href='${link}' value='${label}'>${ty}</option>`;
+        // curObj['html'] = `<li><a href='${link}' value='${ty}'>${label}</a></li>`;
 
         list.push(curObj);
     };
@@ -92,8 +104,8 @@
     // to run a search on fuse, reformats fuses list to fit with typeahead formatting
     // and passes that array onto the typeahead callback
     //
-    function fuseQueryOnList (list) { 
-        return function (query, cb) {   
+    function fuseQueryOnList(list) {
+        return function (query, cb) {
             // Options for fuse
             const options = {
                 shouldSort: true,
@@ -108,49 +120,65 @@
                     weight: 1
                 }]
             };
-            const fuse = new Fuse(list, options); 
+            const fuse = new Fuse(list, options);
             let result;
             result = fuse.search(query);
-            if (result.length > 0 ) {
+            if (result.length > 0) {
                 result.length = (result.length) > 5 ? 5 : result.length;
+                App.options = result;
+                console.log(App);
                 cb(_.map(result, function (elem) {
                     return {value: elem.label};
                 }));
-            } else { 
-                return cb(result);
+            } else {
+                App.options = result;
+                cb(result);
             }
         };
     }
-
+    //
+    // Make an ajax call to load the json, parse it and build saerch ahead on top of the results
     $.ajax({
-      url: '/assets/data/data.json',
-      dataType: 'json',
-      success: function (data) {
-        let json = data;
-        let list = [];
-        formatForSearch(json, list);
+        url: '/assets/data/data.json',
+        dataType: 'json',
+        success: function (data) {
+            let json = data;
+            let list = [];
 
-        $('#search .typeahead').typeahead({
-          hint: true,
-          highlight: true,
-          minLength: 2
-        },
-        {
-          name: 'shrValues',
-          source: fuseQueryOnList(list)
-        });
-        // $('#search').on('input', function (e) {
-        //     result = fuse.search(e.target.value);
-        //     if (result.length > 0 ) {
-        //       result.length = (result.length) > 5 ? 5 : result.length;
-        //       // TODO: Optimize so we don't iterate over once on map, once on join.
-        //       $('#options').html(_.map(result, function (elem) {
-        //         return elem.html;
-        //       }).join('\n'));
-        //     } else { 
-        //       $('#options').html('')
-        //     }
-        // })
-      }
+            formatForSearch(json, list);
+
+            var searchResults = fuseQueryOnList(list);
+
+            $('#search .typeahead').typeahead({
+                    hint: true,
+                    highlight: true,
+                    minLength: 2
+                },
+                {
+                    name: 'shrValues',
+                    // source: fuseQueryOnList(list)
+                    source: searchResults
+                });
+            // $('#search').on('input', function (e) {
+            //     result = fuse.search(e.target.value);
+            //     if (result.length > 0 ) {
+            //       result.length = (result.length) > 5 ? 5 : result.length;
+            //       // TODO: Optimize so we don't iterate over once on map, once on join.
+            //       $('#options').html(_.map(result, function (elem) {
+            //         return elem.html;
+            //       }).join('\n'));
+            //     } else {
+            //       $('#options').html('')
+            //     }
+            // })
+
+            // On search button click, navigate to selected data element from search box
+            $('#searchSubmitButton').on('click', function (e) {
+                e.preventDefault();
+                var redirectLink = _.find(App.options, function (obj) {return obj.label == $('.tt-input')[0].value}).link;
+                window.location.href = redirectLink;
+            })
+        }
     });
 })();
+
