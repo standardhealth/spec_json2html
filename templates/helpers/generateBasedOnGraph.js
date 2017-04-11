e11700b93f34e3357ea1ec15186d84d9bcbbcae8
Nicole ng <nng@mitre.org>
2017-04-11 13:32:38 -0400

    Pulled in Greg's viz prototype. Changed 'hier' to 'namespaces'. The viz does not show up on the index page. Need to fix this

---
+++ templates/helpers/generateBasedOnGraph.js
@@ -0,0 +1,77 @@
+/**
+ * Handlebars Helpers: {{getFilename}}
+ *
+ */
+
+
+// Export helpers
+module.exports.register = function (Handlebars, options, params) {
+  'use strict';
+  
+  var addBasedOnRelationship = function(namespaces, de, deNode, curNamespace) {
+	  var namespace;
+      // var namespaces = _.find(hier.children, {label: "Namespaces"})
+	  var basedOnNode, basedOnDE;
+	  if (de.basedOn) {
+		_.forEach(de.basedOn, function (basedOn) {
+			//basedOnNamespace = result.index[basedOn.namespace];
+			//basedOnNode = basedOnNamespace.index[basedOn.label];
+			if (basedOn.namespace === curNamespace) {
+				basedOnNode = {name: basedOn.label, children: []};
+			} else {
+				basedOnNode = {name: basedOn.label + " (" + basedOn.namespace + ")", children: []};
+			}
+			deNode.children.push(basedOnNode);
+			//console.log(basedOn);
+			//console.log(hier);
+			if (basedOn.namespace) {
+				namespace = _.find(namespaces.children, {label: basedOn.namespace});
+				basedOnDE = _.find(namespace.children, {label: basedOn.label});
+				if (basedOnNode.children.length == 0) {
+					//console.log("more?");
+					//console.log(basedOnDE.basedOn);
+					addBasedOnRelationship(hier, basedOnDE, basedOnNode, curNamespace);
+				}
+			}
+		});
+	  }
+  };
+  
+  var _ = require('lodash');
+  Handlebars.registerHelper('generateBasedOnGraph', function(namespaces, opts) {
+	  var node, index, link, n1, n2;
+	  var result = {};
+	  var nodeList = [];
+	  result.links = [];
+      // var namespaces = _.find(hier.children, {label: "Namespaces"})
+	  _.forEach(namespaces.children, function(namespace) {
+		  _.forEach(namespace.children, function(de) {
+			  node = {label:de.label, r: 2, namespace: namespace, title: namespace.label, key: namespace.label + ":" + de.label, id: nodeList.length, numlinks: 0};
+			  nodeList.push(node);
+		  });
+	  });
+	  index = _.keyBy(nodeList, 'key');
+	  _.forEach(namespaces.children, function(namespace) {
+		  _.forEach(namespace.children, function(de) {
+			  if (de.basedOn) {
+				  //console.log(namespace.label + ":" + de.label);
+				  n1 = index[namespace.label + ":" + de.label];
+				  _.forEach(de.basedOn, function(basedOn) {
+					  if (basedOn.namespace) {
+						  //console.log(basedOn.namespace + ":" + basedOn.label);
+						  n2 = index[basedOn.namespace + ":" + basedOn.label];
+						  n1.numlinks = n1.numlinks + 1;
+						  n2.numlinks = n2.numlinks + 1;
+						  link = {source: n1.id, target: n2.id};
+						  result.links.push(link);
+					  }
+				  });
+			  }
+		  });
+	  });
+
+	  result.nodes = nodeList;
+	  
+      return JSON.stringify(result);
+  });
+};
---
+++ templates/helpers/generateBasedOnHierarchy.js
@@ -0,0 +1,60 @@
+/**
+ * Handlebars Helpers: {{getFilename}}
+ *
+ */
+
+
+// Export helpers
+module.exports.register = function (Handlebars, options, params) {
+  'use strict';
+  
+  var addBasedOnRelationship = function(hier, de, deNode, curNamespace) {
+	  var namespace;
+      var namespaces = _.find(hier.children, {label: "Namespaces"})
+	  var basedOnNode, basedOnDE;
+	  if (de.basedOn) {
+		_.forEach(de.basedOn, function (basedOn) {
+			//basedOnNamespace = result.index[basedOn.namespace];
+			//basedOnNode = basedOnNamespace.index[basedOn.label];
+			if (basedOn.namespace === curNamespace) {
+				basedOnNode = {name: basedOn.label, children: []};
+			} else {
+				basedOnNode = {name: basedOn.label + " (" + basedOn.namespace + ")", children: []};
+			}
+			deNode.children.push(basedOnNode);
+			//console.log(basedOn);
+			//console.log(hier);
+			if (basedOn.namespace) {
+				namespace = _.find(namespaces.children, {label: basedOn.namespace});
+				basedOnDE = _.find(namespace.children, {label: basedOn.label});
+				if (basedOnNode.children.length == 0) {
+					//console.log("more?");
+					//console.log(basedOnDE.basedOn);
+					addBasedOnRelationship(hier, basedOnDE, basedOnNode, curNamespace);
+				}
+			}
+		});
+	  }
+  };
+  
+  var _ = require('lodash');
+  Handlebars.registerHelper('generateBasedOnHierarchy', function(hier, opts) {
+	  var result = {name:"SHR", children: []};
+	  var nsNode, deNode;
+      var namespaces = _.find(hier.children, {label: "Namespaces"})
+	  _.forEach(namespaces.children, function(namespace) {
+		  nsNode = {name: namespace.label, children: []};
+		  result.children.push(nsNode);
+		  _.forEach(namespace.children, function(de) {
+			  deNode = {name:de.label, children: []};
+			  nsNode.children.push(deNode);
+			  //console.log("**** check based on for " + de.label);
+			  addBasedOnRelationship(hier, de, deNode, namespace.label);
+		  });
+		  //nsNode.index = _.keyBy(nsNode.children, 'name');
+	  });
+	  //result.index = _.keyBy(result.children, 'name');
+	  
+      return JSON.stringify(result);
+  });
+};
