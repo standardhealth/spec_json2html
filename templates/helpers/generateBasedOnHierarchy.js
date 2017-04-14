a84d4992c1ff21d8ca6d7a35e05495bb76fc23e2
Nicole ng <nng@mitre.org>
2017-04-14 10:45:29 -0400

    Cleaned up code. Reversed flag for sameNamespace to remove extra code

--- templates/helpers/generateBasedOnHierarchy.js
+++ templates/helpers/generateBasedOnHierarchy.js
@@ -8,28 +8,23 @@
 module.exports.register = function (Handlebars, options, params) {
   'use strict';
 
+	// addBasedonRelationship is not currently being used in the hierarchy view
   var addBasedOnRelationship = function(namespaces, de, deNode, curNamespace) {
 	  var namespace;
-      // var namespaces = _.find(hier.children, {label: "Namespaces"})
 	  var basedOnNode, basedOnDE;
 	  if (de.basedOn) {
 		_.forEach(de.basedOn, function (basedOn) {
-			//basedOnNamespace = result.index[basedOn.namespace];
-			//basedOnNode = basedOnNamespace.index[basedOn.label];
 			if (basedOn.namespace === curNamespace) {
 				basedOnNode = {name: basedOn.label, children: []};
 			} else {
 				basedOnNode = {name: basedOn.label + " (" + basedOn.namespace + ")", children: []};
 			}
 			deNode.children.push(basedOnNode);
-			//console.log(basedOn);
-			//console.log(hier);
+
 			if (basedOn.namespace) {
 				namespace = _.find(namespaces, {label: basedOn.namespace});
 				basedOnDE = _.find(namespace.children, {label: basedOn.label});
 				if (basedOnNode.children.length == 0) {
-					//console.log("more?");
-					//console.log(basedOnDE.basedOn);
 					addBasedOnRelationship(namespaces, basedOnDE, basedOnNode, curNamespace);
 				}
 			}
@@ -39,32 +34,32 @@ module.exports.register = function (Handlebars, options, params) {
   
   var _ = require('lodash');
   Handlebars.registerHelper('generateBasedOnHierarchy', function(namespaces, opts) {
-	  var result = {name:"SHR", sameNamespace: true, children: []};
+	  var result = {name:"SHR", children: []};
 	  var nsNode, entryNode, fieldNode, currentNamespace;
-      // var namespaces = _.find(hier.children, {label: "Namespaces"})
+
 	  _.forEach(namespaces, function(namespace) {
-		  nsNode = {name: namespace.label, sameNamespace: true, children: []};
+		  nsNode = {name: namespace.label, children: []};
 		  result.children.push(nsNode);
 		  _.forEach(namespace.children, function(de) {
 				if (de.isEntry) {
-					entryNode = {name:de.label, sameNamespace: true, children: []};
+					entryNode = {name:de.label, children: []};
 					currentNamespace = de.namespace;
 
 					if (de.value) {
 						if (currentNamespace === de.value.identifier.namespace) {
-							fieldNode = {name: de.value.identifier.label, sameNamespace: true, children: []};
+							fieldNode = {name: de.value.identifier.label, isDifferentNamespace: false, children: []};
 						}
 						else {
-							fieldNode = {name: de.value.identifier.label, sameNamespace: false, children: []};
+							fieldNode = {name: de.value.identifier.label, isDifferentNamespace: true, children: []};
 						}
 						entryNode.children.push(fieldNode);
 					}
 					_.forEach(de.fieldList, function(field) {
 						if (currentNamespace === field.namespace) {
-							fieldNode = {name: field.label, sameNamespace: true, children: []};
+							fieldNode = {name: field.label, isDifferentNamespace: false, children: []};
 						}
 						else {
-							fieldNode = {name: field.label, sameNamespace: false, children: []};
+							fieldNode = {name: field.label, isDifferentNamespace: true, children: []};
 						}
 
 						entryNode.children.push(fieldNode);
